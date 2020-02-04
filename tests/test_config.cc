6cd01cec70d562d6965f0be2a04ae4c6c9ca12ad
zjlian <zjlianzjlianzjlian@outlook.com>
2020-02-05 01:03:12 +0800

    test

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -1,6 +1,7 @@
 #include "src/config.h"
 #include "src/log.h"
 #include "yaml-cpp/yaml.h"
+#include <iostream>
 
 int main() {
     auto int_value_config = zjl::Config::Lookup("config", 55555);
@@ -8,7 +9,14 @@ int main() {
     LOG_FMT_DEBUG(GET_ROOT_LOGGER(), "%d", int_value_config->getValue());
     LOG_FMT_DEBUG(GET_ROOT_LOGGER(), "%d", zjl::Config::Lookup<int>("config")->getValue());
 
+    YAML::Node config;
+    try {
+        config = YAML::LoadFile("../config.yml");
+    } catch (const std::exception& e) {
+        LOG_FMT_ERROR(GET_ROOT_LOGGER(), "文件加载失败：%s", e.what());
+    }
 
+    std::cout << config;
 
     return 0;
 }
