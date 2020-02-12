5b435be4a6fd063203d6ead6118d0939ad68272d
zjlian <zjlianzjlianzjlian@outlook.com>
2020-02-13 00:01:15 +0800

    test: 修改测试代码

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -1,59 +1,43 @@
 #include "src/config.h"
 #include "src/log.h"
 #include "yaml-cpp/yaml.h"
+#include <cstdint>
 #include <iostream>
 
-void loadConfig(const YAML::Node& root_node) {
-#define RANGE(OBJ) OBJ.begin(), OBJ.end()
-    auto logs_node = root_node["logs"];
-    if (!logs_node.IsNull()) {
-        std::for_each(RANGE(logs_node), [](const YAML::Node& log_item) {
-            std::cout << log_item["name"] << std::endl;
-            std::cout << log_item["level"] << std::endl;
-            std::cout << log_item["formatter"] << std::endl;
-            // std::cout << log_item["appender"] << std::endl;
-            auto appender_list = log_item["appender"];
-            std::for_each(RANGE(appender_list), [](const YAML::Node& appender) {
-                std::cout << appender["type"] << std::endl;
-            });
-        });
-    }
-#undef RANGE
-}
+// 创建默认配置项
+auto config_system_port = zjl::Config::Lookup<uint16_t>("system.port", 6666);
+auto config_logger_list = zjl::Config::Lookup<std::vector<std::string>>("logs", {"sb", "sb"});
 
-int main() {
-    auto int_value_config = zjl::Config::Lookup("config", 55555);
-    LOG_FMT_DEBUG(GET_ROOT_LOGGER(), "%s", typeid(int_value_config).name());
-    LOG_FMT_DEBUG(GET_ROOT_LOGGER(), "%d", int_value_config->getValue());
-    LOG_FMT_DEBUG(GET_ROOT_LOGGER(), "%d", zjl::Config::Lookup<int>("config")->getValue());
-
-    zjl::Config::Lookup("system.port", 6666);
-    auto port = zjl::Config::Lookup("system.port");
-    std::cout << port << std::endl;
-
+// 测试通过解析 yaml 文件更新配置项
+void TEST_loadConfig(const std::string& path) {
     YAML::Node config;
     try {
-        config = YAML::LoadFile("/home/workspace/c/server-framework/config.yml");
+        config = YAML::LoadFile(path);
     } catch (const std::exception& e) {
         LOG_FMT_ERROR(GET_ROOT_LOGGER(), "文件加载失败：%s", e.what());
     }
     zjl::Config::LoadFromYAML(config);
-    std::cout << port << std::endl;
+}
 
-    auto log_name = zjl::Config::Lookup("logs.0.name");
-    if (log_name) {
-        LOG_DEBUG(GET_ROOT_LOGGER(), "has value");
-        std::cout << log_name << std::endl;
-    } else {
+// 测试获取默认的配置项
+void TEST_defaultConfig() {
+    std::cout << config_system_port.get() << std::endl;
+    std::cout << config_logger_list.get() << std::endl;
+}
+
+// 测试获取不存在的配置项
+void TEST_nonexistentConfig() {
+    auto log_name = zjl::Config::Lookup("nonexistent");
+    if (!log_name) {
         LOG_ERROR(GET_ROOT_LOGGER(), "non value");
     }
+}
 
-#define TEST(str) std::cout << zjl::Config::Lookup(str) << std::endl;
-    TEST("logs.0.level")
-    TEST("logs.0.formatter")
-    TEST("logs.0.appender.0.type")
-    TEST("logs.0.appender.0.file")
-    TEST("nonexistent")
-#undef TEST
+int main() {
+    TEST_defaultConfig();
+    TEST_loadConfig("/home/workspace/c/server-framework/config.yml");
+    TEST_defaultConfig();
+    TEST_nonexistentConfig();
+
     return 0;
 }
