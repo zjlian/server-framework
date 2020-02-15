2e6e144abffed9bcb87de1b6b7d678e45e446ab5
zjlian <zjlianzjlianzjlian@outlook.com>
2020-02-15 15:40:05 +0800

    fix

--- src/config.cpp
+++ src/config.cpp
@@ -5,7 +5,7 @@ namespace zjl {
 Config::ConfigVarMap Config::s_data;
 
 std::ostream& operator<<(std::ostream& out, const ConfigVarBase& cvb) {
-    out << cvb->getName() << ": " << cvb->toString();
+    out << cvb.getName() << ": " << cvb.toString();
     return out;
 }
 }
