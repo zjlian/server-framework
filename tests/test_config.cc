043ecfd1a839b80f41a384dadd82697fd3c9e02b
zjlian <zjlianzjlianzjlian@outlook.com>
2020-02-15 22:25:01 +0800

    test

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -4,12 +4,22 @@
 #include <cstdint>
 #include <iostream>
 #include <list>
+#include <map>
 #include <vector>
 
 // 创建默认配置项
 auto config_system_port = zjl::Config::Lookup<int>("system.port", 6666);
-auto config_test_list = zjl::Config::Lookup<std::vector<std::string>>("test_list", std::vector<std::string>{"vector", "string"});
-auto config_test_linklist = zjl::Config::Lookup<std::list<std::string>>("test_linklist", std::list<std::string>{"list", "string"});
+auto config_test_list = zjl::Config::Lookup<std::vector<std::string>>(
+    "test_list", std::vector<std::string>{"vector", "string"});
+auto config_test_linklist = zjl::Config::Lookup<std::list<std::string>>(
+    "test_linklist", std::list<std::string>{"list", "string"});
+auto config_test_map = zjl::Config::Lookup<std::map<std::string, std::string>>(
+    "test_map", std::map<std::string, std::string>{
+                    std::make_pair("map1", "srting"),
+                    std::make_pair("map2", "srting"),
+                    std::make_pair("map3", "srting")});
+auto config_test_set = zjl::Config::Lookup<std::set<int>>(
+    "test_set", std::set<int>{10, 20, 30});
 
 // 测试通过解析 yaml 文件更新配置项
 void TEST_loadConfig(const std::string& path) {
@@ -29,6 +39,8 @@ void TEST_ConfigVarToString() {
     std::cout << *config_system_port << std::endl;
     std::cout << *config_test_list << std::endl;
     std::cout << *config_test_linklist << std::endl;
+    std::cout << *config_test_map << std::endl;
+    std::cout << *config_test_set << std::endl;
 }
 
 // 测试获取并使用配置的值
@@ -44,7 +56,18 @@ void TEST_GetConfigVarValue() {
 
     TSEQ(config_test_list);
     TSEQ(config_test_linklist);
+    TSEQ(config_test_set);
 #undef TSEQ
+// 遍历映射容器的宏
+#define TMAP(config_var)                                                \
+    std::cout << "name = " << config_var->getName() << "; value = ";    \
+    for (const auto& pair : config_var->getValue()) {                   \
+        std::cout << "<" << pair.first << ", " << pair.second << ">, "; \
+    }                                                                   \
+    std::cout << std::endl;
+
+    TMAP(config_test_map);
+#undef TMAP
 }
 
 // 测试获取不存在的配置项
