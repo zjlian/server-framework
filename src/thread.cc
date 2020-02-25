b57dae16cc4f56b281990e6cdff2d0e92527e636
zjlian <zjlianzjlianzjlian@outlook.com>
2020-02-25 21:49:52 +0800

    feat: 增加 Thread 类

---
+++ src/thread.cc
@@ -0,0 +1,96 @@
+#include "thread.h"
+#include "log.h"
+#include <exception>
+
+namespace zjl {
+
+/**
+ * 线程局部变量
+*/
+// 记录当前线程的 Thread 实例的指针
+static thread_local Thread* t_thread = nullptr;
+// 记录当前线程的线程名
+static thread_local std::string t_thread_name = "UNKNOWN";
+
+static Logger::ptr system_logger = GET_LOGGER("system");
+
+/**
+ * Thread 类的实现
+*/
+Thread*
+Thread::GetThis() {
+    return t_thread;
+}
+
+const std::string&
+Thread::GetThisThreadName() {
+    return t_thread_name;
+}
+
+void SetThisThreadName(const std::string& name) {
+    if (t_thread) {
+        t_thread->setName(name);
+    }
+    t_thread_name = std::move(name);
+}
+
+Thread::Thread(std::function<void()> callback, std::string name)
+    : m_id(-1), m_name(name), m_thread(0) {
+    // 调用 pthread_create 创建新线程
+    // 将 Thread 实例作为参数传递给静态方法 Thread::Run, 在静态方法内部调用 Thread 实例的 callback
+    int result = pthread_create(&m_thread, nullptr, &Thread::Run, this);
+    if (result) {
+        LOG_FMT_ERROR(
+            system_logger,
+            "pthread_create() 线程创建失败, 线程名 = %s, 错误码 = %d",
+            name.c_str(), result);
+        throw std::system_error();
+    }
+}
+
+Thread::~Thread() {
+    // 如果线程存在，将线程与主线程分离
+    if (m_thread) {
+        pthread_detach(m_thread);
+    }
+}
+
+id_t Thread::getId() const {
+    return m_id;
+}
+
+const std::string&
+Thread::getName() const {
+    return m_name;
+}
+
+void Thread::setName(const std::string& name) {
+    m_name = name;
+}
+
+void Thread::join() {
+    if (m_thread) {
+        int result = pthread_join(m_thread, nullptr);
+        if (result) {
+            LOG_FMT_ERROR(
+                system_logger,
+                "pthread_join() 执行失败，错误码 = %d",
+                result);
+            throw std::system_error();
+        }
+    }
+}
+
+void* Thread::Run(void* arg) {
+    Thread* thread = (Thread*)arg;
+    // 赋值将当前线程的局部变量 t_thread
+    t_thread = thread;
+    thread->m_id = getThreadID();
+    // 设置 pthread 线程的线程名
+    pthread_setname_np(pthread_self(), thread->m_name.substr(0, 15).c_str());
+    std::function<void()> callback;
+    callback.swap(thread->m_callback);
+    callback();
+    return 0;
+}
+}
---
+++ src/thread.h
@@ -0,0 +1,61 @@
+#ifndef SERVER_FRAMEWORK_THREAD_H
+#define SERVER_FRAMEWORK_THREAD_H
+
+#include <functional>
+#include <memory>
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+namespace zjl {
+
+/**
+ * @brief 线程类
+ * 基于 pthread 封装的
+*/
+class Thread {
+public:
+    typedef std::shared_ptr<Thread> ptr;
+    typedef std::unique_ptr<Thread> uptr;
+
+    Thread(std::function<void()> callback, std::string name);
+    ~Thread();
+    // 获取线程 id
+    id_t getId() const;
+    // 获取线程名称
+    const std::string& getName() const;
+    // 设置线程名称
+    void setName(const std::string& name);
+    // 将线程并入主线程
+    void join();
+
+public:
+    // 获取当前线程
+    static Thread* GetThis();
+    // 获取当前运行线程的名称
+    static const std::string& GetThisThreadName();
+    // 设置当前运行线程的名称
+    static void SetThisThreadName(const std::string& name);
+    // 启动线程, 接收 Thread*
+    static void* Run(void* arg);
+
+private:
+    // 禁用 Thread 类的拷贝移动等操作
+    Thread(const Thread&) = delete;
+    Thread(const Thread&&) = delete;
+    Thread& operator=(const Thread&) = delete;
+    Thread& operator=(const Thread&&) = delete;
+
+private:
+    // 系统线程 id, 通过 syscall() 获取
+    pid_t m_id;
+    // 线程名称
+    std::string m_name;
+    // pthread 线程 id
+    pthread_t m_thread;
+    // 线程执行的函数
+    std::function<void()> m_callback;
+};
+}
+
+#endif
