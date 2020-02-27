de25d41cc47a661609d272c86d9f209503c77237
zjlian <zjlianzjlianzjlian@outlook.com>
2020-02-27 19:19:33 +0800

    feat: 增加信号量 Semaphore 类

--- src/thread.cc
+++ src/thread.cc
@@ -1,5 +1,6 @@
 #include "thread.h"
 #include "log.h"
+#include <assert.h>
 #include <exception>
 #include <unistd.h>
 
@@ -9,18 +10,108 @@ namespace zjl {
  * 线程局部变量
 */
 // 记录当前线程的 Thread 实例的指针
-static thread_local Thread* t_thread = nullptr;
+// static thread_local Thread* t_thread = nullptr;
+static thread_local pid_t t_tid = 0;
 // 记录当前线程的线程名
 static thread_local std::string t_thread_name = "UNKNOWN";
 
 static Logger::ptr system_logger = GET_LOGGER("system");
 
 /**
- * Thread 类的实现
+ * =========================================
+ * Semaphore 类的实现
+ * =========================================
 */
-Thread*
-Thread::GetThis() {
-    return t_thread;
+
+Semaphore::Semaphore(uint32_t count) {
+    if (sem_init(&m_semaphore, 0, count)) {
+        LOG_FATAL(
+            system_logger,
+            "sem_init() 初始化信号量失败");
+        throw std::system_error();
+        // TODO 失败时是否应该直接结束程序？
+    }
+}
+
+Semaphore::~Semaphore() {
+    sem_destroy(&m_semaphore);
+}
+
+void Semaphore::wait() {
+    if (sem_wait(&m_semaphore)) {
+        LOG_FATAL(
+            system_logger,
+            "sem_wait() 异常");
+        throw std::system_error();
+        // TODO 失败时是否应该直接结束程序？
+    }
+}
+
+void Semaphore::notify() {
+    if (sem_post(&m_semaphore)) {
+        LOG_FATAL(
+            system_logger,
+            "sem_post() 异常");
+        throw std::system_error();
+        // TODO 失败时是否应该直接结束程序？
+    }
+}
+
+/**
+ * @brief 线程数据类
+ * 封装线程执行需要的数据
+*/
+struct ThreadData {
+    typedef Thread::ThreadFunc ThreadFunc;
+    ThreadFunc m_callback;
+    std::string m_name;
+    pid_t* m_id;
+    Semaphore* m_semaphore;
+
+    ThreadData(ThreadFunc func,
+               const std::string& name,
+               pid_t* tid,
+               Semaphore* sem)
+        : m_callback(std::move(func)),
+          m_name(name),
+          m_id(tid),
+          m_semaphore(sem) {}
+
+    void runInThread() {
+        // 获取系统线程 id
+        *m_id = GetThreadID();
+        m_id = nullptr;
+        // 信号量 +1，通知主线程，子线程启动成功
+        m_semaphore->notify();
+        m_semaphore = nullptr;
+        t_tid = GetThreadID();
+        t_thread_name = m_name.empty() ? "UNKNOWN" : m_name;
+        pthread_setname_np(pthread_self(), m_name.substr(0, 15).c_str());
+        try {
+            m_callback();
+        } catch (const std::exception& e) {
+            LOG_FMT_FATAL(
+                system_logger,
+                "线程执行异常，name = %s, 原因：%s",
+                m_name.c_str(),
+                e.what());
+            abort();
+        }
+    }
+};
+
+/**
+ * ===================================================
+ * Thread 类的实现
+ * ===================================================
+*/
+// Thread*
+// Thread::GetThis() {
+//     return t_thread;
+// }
+
+pid_t Thread::GetThisId() {
+    return t_tid;
 }
 
 const std::string&
@@ -29,32 +120,45 @@ Thread::GetThisThreadName() {
 }
 
 void SetThisThreadName(const std::string& name) {
-    if (t_thread) {
-        t_thread->setName(name);
-    }
+    // if (t_thread) {
+    //     t_thread->setName(name);
+    // }
     t_thread_name = std::move(name);
 }
 
-Thread::Thread(std::function<void()> callback, std::string name)
-    : m_id(-1), m_name(name), m_thread(0), m_callback(callback) {
+Thread::Thread(ThreadFunc callback, std::string name)
+    : m_id(-1),
+      m_name(name),
+      m_thread(0),
+      m_callback(callback),
+      m_semaphore(0),
+      m_started(true),
+      m_joined(false) {
     // 调用 pthread_create 创建新线程
-    // 将 Thread 实例作为参数传递给静态方法 Thread::Run, 在静态方法内部调用 Thread 实例的 callback
-    int result = pthread_create(&m_thread, nullptr, &Thread::Run, this);
+    ThreadData* data =
+        new ThreadData(m_callback, m_name, &m_id, &m_semaphore);
+    int result = pthread_create(&m_thread, nullptr, &Thread::Run, data);
     if (result) {
-        LOG_FMT_ERROR(
+        m_started = false;
+        delete data;
+        LOG_FMT_FATAL(
             system_logger,
             "pthread_create() 线程创建失败, 线程名 = %s, 错误码 = %d",
             name.c_str(), result);
         throw std::system_error();
+    } else {
+        // 等待子线程启动
+        m_semaphore.wait();
+        // m_id 储存系统线程 id, 如果小于0，说明线程启动失败
+        assert(m_id > 0);
     }
 }
 
 Thread::~Thread() {
-    // 如果线程存在，将线程与主线程分离
-    if (m_thread) {
+    // 如果线程有效且位 join，将线程与主线程分离
+    if (m_started && !m_joined) {
         pthread_detach(m_thread);
     }
-    // TODO 实例析构后，线程无法执行
 }
 
 pid_t Thread::getId() const {
@@ -70,37 +174,16 @@ void Thread::setName(const std::string& name) {
     m_name = name;
 }
 
-void Thread::join() {
-    if (m_thread) {
-        int result = pthread_join(m_thread, nullptr);
-        if (result) {
-            LOG_FMT_ERROR(
-                system_logger,
-                "pthread_join() 执行失败，错误码 = %d",
-                result);
-            throw std::system_error();
-        }
-        m_thread = 0;
-    }
+int Thread::join() {
+    assert(m_started);
+    assert(!m_joined);
+    m_joined = true;
+    return pthread_join(m_thread, nullptr);
 }
 
 void* Thread::Run(void* arg) {
-    Thread* thread = (Thread*)arg;
-    if (!thread->m_callback) {
-        LOG_ERROR(
-            system_logger,
-            "Thread::Run() 异常，Thread 实例已被析构,无法调用 callback");
-        throw std::logic_error("Thread 实例已被析构,无法调用 callback");
-    }
-    // 赋值将当前线程的局部变量 t_thread
-    t_thread = thread;
-    t_thread_name = thread->m_name;
-    thread->m_id = GetThreadID();
-    // 设置 pthread 线程的线程名
-    pthread_setname_np(pthread_self(), thread->m_name.substr(0, 15).c_str());
-    std::function<void()> callback;
-    callback.swap(thread->m_callback);
-    callback();
+    std::unique_ptr<ThreadData> data((ThreadData*)arg);
+    data->runInThread();
     return 0;
 }
 }
--- src/thread.h
+++ src/thread.h
@@ -1,24 +1,44 @@
 #ifndef SERVER_FRAMEWORK_THREAD_H
 #define SERVER_FRAMEWORK_THREAD_H
 
+#include "util.h"
 #include <functional>
 #include <memory>
 #include <pthread.h>
+#include <semaphore.h>
+#include <stdint.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 namespace zjl {
 
 /**
+ * @brief 对 semaphore.h 信号量的简单封装
+*/
+class Semaphore : public noncopyable {
+public:
+    explicit Semaphore(uint32_t count);
+    ~Semaphore();
+    // -1，值为零时阻塞
+    void wait();
+    //  +1
+    void notify();
+
+private:
+    sem_t m_semaphore;
+};
+
+/**
  * @brief 线程类
  * 基于 pthread 封装的
 */
-class Thread {
+class Thread : public noncopyable {
 public:
     typedef std::shared_ptr<Thread> ptr;
     typedef std::unique_ptr<Thread> uptr;
+    typedef std::function<void()> ThreadFunc;
 
-    Thread(std::function<void()> callback, std::string name);
+    Thread(ThreadFunc callback, std::string name);
     ~Thread();
     // 获取线程 id
     pid_t getId() const;
@@ -27,11 +47,13 @@ public:
     // 设置线程名称
     void setName(const std::string& name);
     // 将线程并入主线程
-    void join();
+    int join();
 
 public:
     // 获取当前线程
-    static Thread* GetThis();
+    // static Thread* GetThis();
+    // 获取当前线程的系统线程 id
+    static pid_t GetThisId();
     // 获取当前运行线程的名称
     static const std::string& GetThisThreadName();
     // 设置当前运行线程的名称
@@ -41,10 +63,10 @@ public:
 
 private:
     // 禁用 Thread 类的拷贝移动等操作
-    Thread(const Thread&) = delete;
-    Thread(const Thread&&) = delete;
-    Thread& operator=(const Thread&) = delete;
-    Thread& operator=(const Thread&&) = delete;
+    // Thread(const Thread&) = delete;
+    // Thread(const Thread&&) = delete;
+    // Thread& operator=(const Thread&) = delete;
+    // Thread& operator=(const Thread&&) = delete;
 
 private:
     // 系统线程 id, 通过 syscall() 获取
@@ -54,7 +76,12 @@ private:
     // pthread 线程 id
     pthread_t m_thread;
     // 线程执行的函数
-    std::function<void()> m_callback;
+    ThreadFunc m_callback;
+    // 控制线程启动的信号量
+    Semaphore m_semaphore;
+    // 线程状态
+    bool m_started;
+    bool m_joined;
 };
 }
 
