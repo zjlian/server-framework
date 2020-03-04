31188629128244e29e65a6b13644e86526c573ec
zjlian <zjlianzjlianzjlian@outlook.com>
2020-03-04 19:16:47 +0000

    feat: 增加对 excinfo.h 的 backtrace 的简单封装

--- src/util.cpp
+++ src/util.cpp
@@ -1,4 +1,6 @@
 #include "util.h"
+#include <execinfo.h>
+#include <iostream>
 
 namespace zjl
 {
@@ -12,4 +14,28 @@ int GetFiberID()
 {
     return 0;
 }
+
+void Backtrace(std::vector<std::string>& out, int size, int skip)
+{
+    void** void_ptr_list = (void**)malloc(sizeof(void*) * size);
+    int call_stack_count = ::backtrace(void_ptr_list, size);
+    char** string_list = ::backtrace_symbols(void_ptr_list, call_stack_count);
+    if (string_list == NULL)
+    {
+        std::cerr << "Backtrace() exception, 调用栈获取失败" << std::endl;
+    }
+    for (int i = skip; i < call_stack_count; i++)
+    {
+        out.push_back(string_list[i]);
+    }
+    // backtrace_symbols() 返回 malloc 分配的内存指针，需要 free
+    free(string_list);
+    free(void_ptr_list);
+}
+std::string BacktraceToSring(int size, int skip)
+{
+    // TODO
+    return "";
+}
+
 } // namespace zjl
--- src/util.h
+++ src/util.h
@@ -6,17 +6,35 @@
 #include <cinttypes>
 #include <memory>
 #include <pthread.h>
+#include <string>
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <vector>
 
 namespace zjl
 {
 
 // 获取linux下线程的唯一id
 int GetThreadID();
+
 // 获取协程id
 int GetFiberID();
+
+/**
+ * @brief 以 vector 的形式获取调用栈
+ * @param out 获取的调用栈
+ * @param size 获取调用栈的最大层数
+ * @param skip 省略最近 n 层调用栈，一般用于传 1，忽略获取 Backtrace() 本身的调用栈
+*/
+void Backtrace(std::vector<std::string>& out, int size, int skip);
+
+/**
+ * @brief 获取调用栈字符串
+ * @param size 获取调用栈的最大层数
+ * @param skip 省略最近 n 层调用栈，一般用于传 1，忽略获取 Backtrace() 本身的调用栈
+*/
+std::string BacktraceToSring(int size, int skip);
 
 } // namespace zjl
 #endif
