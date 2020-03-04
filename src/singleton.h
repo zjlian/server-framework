5a61e0492791ea923e2fd4e2c34efb17adb8e9a4
zjlian <zjlianzjlianzjlian@outlook.com>
2020-03-04 17:02:29 +0000

    refactor: Singleton 独立文件

---
+++ src/singleton.h
@@ -0,0 +1,44 @@
+#ifndef SERVER_FRAMEWORK_SINGLETON_H
+#define SERVER_FRAMEWORK_SINGLETON_H
+
+namespace zjl
+{
+
+/**
+ * @brief 简单的单例包装类
+ * 调用 Singleton::GetInstance 返回被包装类型的原生指针
+*/
+template <class T>
+class Singleton final
+{
+public:
+    static T* GetInstance()
+    {
+        static T ins;
+        return &ins;
+    }
+
+private:
+    Singleton() = default;
+};
+
+/**
+ * @brief 简单的单例包装类
+ * 调用 Singleton::GetInstance 返回被包装类型的 std::shared_ptr 智能指针
+*/
+template <class T>
+class SingletonPtr final
+{
+public:
+    static std::shared_ptr<T> GetInstance()
+    {
+        static auto ins = std::make_shared<T>();
+        return ins;
+    }
+
+private:
+    SingletonPtr() = default;
+};
+} // namespace zjl
+
+#endif
--- src/util.h
+++ src/util.h
@@ -1,6 +1,8 @@
 #ifndef SERVER_FRAMEWORK_UTIL_H
 #define SERVER_FRAMEWORK_UTIL_H
 
+#include "noncopyable.h"
+#include "singleton.h"
 #include <cinttypes>
 #include <memory>
 #include <pthread.h>
@@ -15,42 +17,6 @@ namespace zjl
 int GetThreadID();
 // 获取协程id
 int GetFiberID();
-
-/**
- * @brief 简单的单例包装类
- * 调用 Singleton::GetInstance 返回被包装类型的原生指针
-*/
-template <class T>
-class Singleton final
-{
-public:
-    static T* GetInstance()
-    {
-        static T ins;
-        return &ins;
-    }
-
-private:
-    Singleton() = default;
-};
-
-/**
- * @brief 简单的单例包装类
- * 调用 Singleton::GetInstance 返回被包装类型的 std::shared_ptr 智能指针
-*/
-template <class T>
-class SingletonPtr final
-{
-public:
-    static std::shared_ptr<T> GetInstance()
-    {
-        static auto ins = std::make_shared<T>();
-        return ins;
-    }
-
-private:
-    SingletonPtr() = default;
-};
 
 } // namespace zjl
 #endif
