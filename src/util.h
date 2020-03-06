97f8f98aa7268cc56d849d552cdb3913be48335d
zjlian <zjlianzjlianzjlian@outlook.com>
2020-03-06 16:17:02 +0000

    refactor: Backtrace() 添加默认参数

--- src/util.h
+++ src/util.h
@@ -24,17 +24,17 @@ int GetFiberID();
 /**
  * @brief 以 vector 的形式获取调用栈
  * @param out 获取的调用栈
- * @param size 获取调用栈的最大层数
+ * @param size 获取调用栈的最大层数，默认值为 200
  * @param skip 省略最近 n 层调用栈，默认值为 1，忽略获取 Backtrace() 本身的调用栈
 */
-void Backtrace(std::vector<std::string>& out, int size, int skip = 1);
+void Backtrace(std::vector<std::string>& out, int size = 200, int skip = 1);
 
 /**
  * @brief 获取调用栈字符串，内部调用 Backtrace()
- * @param size 获取调用栈的最大层数
+ * @param size 获取调用栈的最大层数，默认值为 200
  * @param skip 省略最近 n 层调用栈，默认值为 2，忽略获取 BacktraceToSring() 和 Backtrace() 的调用栈
 */
-std::string BacktraceToSring(int size, int skip = 2);
+std::string BacktraceToSring(int size = 200, int skip = 2);
 
 } // namespace zjl
 #endif
