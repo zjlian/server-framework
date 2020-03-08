8ee5a3c95206fb28f61917525ed732332d49215e
zjlian <zjlianzjlianzjlian@outlook.com>
2020-03-08 23:33:18 +0800

    feat: 完善协程的实现

--- src/fiber.cc
+++ src/fiber.cc
@@ -1,31 +1,13 @@
 #include "fiber.h"
-#include "config.h"
 #include "exception.h"
-#include <assert.h>
-#include <atomic>
+#include "log.h"
+#include <cassert>
+#include <cerrno>
 #include <cstring>
-#include <errno.h>
+#include <utility>
 
 namespace zjl
 {
-
-namespace FiberInfo
-{
-
-// 最后一个协程的 id
-static std::atomic_uint64_t s_fiber_id{0};
-// 存在的协程数量
-static std::atomic_uint64_t s_fiber_count{0};
-
-// 当前协程
-static thread_local Fiber* t_fiber = nullptr;
-// 主协程
-static thread_local Fiber::ptr t_master_fiber{};
-
-// 协程栈大小配置项
-static ConfigVar<uint64_t>::ptr g_fiber_stack_size =
-    Config::Lookup<uint64_t>("fiber.stack_size", 1024 * 1024);
-} // namespace FiberInfo
 
 /**
  * @brief 对 malloc/free 简单封装的内存分配器
@@ -65,7 +47,7 @@ Fiber::Fiber()
 {
     SetThis(this);
     // 获取上下文对象的副本
-    if (getcontext(&m_ctx) == -1)
+    if (getcontext(&m_ctx))
     {
         throw Exception(std::string(::strerror(errno)));
     }
@@ -79,7 +61,7 @@ Fiber::Fiber(FiberFunc callback, size_t stack_size)
       m_state(INIT),
       m_ctx(),
       m_stack(nullptr),
-      m_callback(callback)
+      m_callback(std::move(callback))
 {
     // 如果传入的 stack_size 为 0，使用配置项 "fiber.stack_size" 设置的值
     if (m_stack_size == 0)
@@ -87,7 +69,7 @@ Fiber::Fiber(FiberFunc callback, size_t stack_size)
         m_stack_size = FiberInfo::g_fiber_stack_size->getValue();
     }
     // 获取上下文对象的副本
-    if (getcontext(&m_ctx) == -1)
+    if (getcontext(&m_ctx))
     {
         throw Exception(std::string(::strerror(errno)));
     }
@@ -104,18 +86,18 @@ Fiber::Fiber(FiberFunc callback, size_t stack_size)
 
 Fiber::~Fiber()
 {
-    if (m_stack) // 存在栈，说明是子协程
+    if (m_stack) // 存在栈，说明是子协程，释放申请的协程栈空间
     {
         // 只有子协程未被启动或者执行结束，才能被析构，否则属于程序错误
-        assert(m_state == INIT || m_state == TERM);
+        assert(m_state == INIT || m_state == TERM || m_state == EXCEPTION);
         StackAllocator::Dealloc(m_stack, m_stack_size);
     }
-    else // 否则是主协程
+    else // 否则是 master fiber
     {
+        // master fiber 不存在执行函数
         assert(!m_callback);
         assert(m_state == EXEC);
-        Fiber* current_fiber = FiberInfo::t_fiber;
-        if (current_fiber == this)
+        if (FiberInfo::t_fiber == this)
         {
             SetThis(nullptr);
         }
@@ -124,38 +106,119 @@ Fiber::~Fiber()
 
 void Fiber::reset(FiberFunc callback)
 {
+    assert(m_stack);
+    assert(m_state == INIT || m_state == TERM || m_state == EXCEPTION);
+    m_callback = std::move(callback);
+    if (getcontext(&m_ctx))
+    {
+        throw Exception(std::string(::strerror(errno)));
+    }
+    m_ctx.uc_link = nullptr;
+    m_ctx.uc_stack.ss_sp = m_stack;
+    m_ctx.uc_stack.ss_size = m_stack_size;
+    makecontext(&m_ctx, &Fiber::MainFunc, 0);
+    m_state = INIT;
 }
 
 void Fiber::swapIn()
 {
+//    assert(m_state != EXEC);
+    SetThis(this);
+    m_state = EXEC;
+    // 挂起 master fiber，切换到当前 fiber
+    if (swapcontext(&(FiberInfo::t_master_fiber->m_ctx), &m_ctx))
+    {
+        throw Exception(std::string(::strerror(errno)));
+    }
 }
 
 void Fiber::swapOut()
 {
+    SetThis(FiberInfo::t_master_fiber.get());
+    // 挂起当前 fiber，切换到 master fiber
+    if (swapcontext(&m_ctx, &(FiberInfo::t_master_fiber->m_ctx)))
+    {
+        throw Exception(std::string(::strerror(errno)));
+    }
 }
 
 Fiber::ptr Fiber::GetThis()
 {
+    if (FiberInfo::t_fiber)
+    {
+        // 调用 std::enable_shared_from_this::shared_from_this() 获取对象 this 的智能指针
+        return FiberInfo::t_fiber->shared_from_this();
+    }
+    // 当 FiberInfo::t_fiber 是 nullptr 时，说明该线程不存在 master fiber
+    // 初始化 master_fiber
+    Fiber::ptr first_fiber(new Fiber());
+    FiberInfo::t_master_fiber.swap(first_fiber);
+    return FiberInfo::t_master_fiber->shared_from_this();
 }
 
-void SetThis(Fiber* fiber)
+void Fiber::SetThis(Fiber* fiber)
 {
+    FiberInfo::t_fiber = fiber;
 }
 
 void Fiber::YieldToReady()
 {
+    auto current_fiber = GetThis();
+    current_fiber->m_state = READY;
+    current_fiber->swapOut();
 }
 
 void Fiber::YieldToHold()
 {
+    auto current_fiber = GetThis();
+    current_fiber->m_state = HOLD;
+    current_fiber->swapOut();
 }
 
 uint64_t Fiber::TotalFiber()
 {
+    return FiberInfo::s_fiber_count;
+}
+
+uint64_t Fiber::GetFiberID()
+{
+    if (FiberInfo::t_fiber)
+    {
+        return FiberInfo::t_fiber->getID();
+    }
+    return 0;
 }
 
 void Fiber::MainFunc()
 {
+    auto current_fiber = GetThis();
+    try
+    {
+        current_fiber->m_callback();
+        current_fiber->m_callback = nullptr;
+        current_fiber->m_state = TERM;
+    }
+    catch (zjl::Exception& e)
+    {
+        LOG_FMT_ERROR(
+            GET_LOGGER("system"),
+            "Fiber exception: %s, call stack:\n%s",
+            e.what(),
+            e.stackTrace());
+    }
+    catch (std::exception& e)
+    {
+        LOG_FMT_ERROR(
+            GET_LOGGER("system"),
+            "Fiber exception: %s",
+            e.what());
+    }
+    catch (...)
+    {
+        LOG_ERROR(
+            GET_LOGGER("system"),
+            "Fiber exception");
+    }
 }
 
 } // namespace zjl
--- src/fiber.h
+++ src/fiber.h
@@ -1,7 +1,9 @@
 #ifndef SERVER_FRAMEWORK_FIBER_H
 #define SERVER_FRAMEWORK_FIBER_H
 
+#include "config.h"
 #include "thread.h"
+#include <atomic>
 #include <functional>
 #include <memory>
 #include <ucontext.h>
@@ -19,40 +21,51 @@ public:
     using uptr = std::unique_ptr<Fiber>;
     using FiberFunc = std::function<void()>;
 
-    // 协程状态
+    // 协程状态，用于调度
     enum State
     {
-        INIT,  // 初始化
-        HOLD,  // 挂起
-        EXEC,  // 执行
-        TERM,  // 结束
-        READY, // 预备
+        INIT,     // 初始化
+        HOLD,     // 挂起
+        EXEC,     // 执行
+        TERM,     // 结束
+        READY,    // 预备
+        EXCEPTION // 异常
     };
 
 public:
-    Fiber(FiberFunc callback, size_t stack_size);
+    /**
+     * @brief 创建新协程
+     * @param callback 协程执行函数
+     * @param 协程栈大小，如果传 0，使用配置项 "fiber.stack_size" 定义的值
+     * */
+    Fiber(FiberFunc callback, size_t stack_size = 0);
     ~Fiber();
-
+    // 更换协程执行函数
     void reset(FiberFunc callback);
-    // 换入协程，开始执行
+    // 换入协程
     void swapIn();
     // 挂起协程
     void swapOut();
+    // 获取协程 id
+    uint64_t getID() const { return m_id; }
 
 private:
+    // 用于创建 master fiber
     Fiber();
 
 public:
-    // 获取当前协程对象
+    // 获取当前 fiber 的指针指针
     static Fiber::ptr GetThis();
-    // 设置当前协程对象
+    // 设置当前 fiber
     static void SetThis(Fiber* fiber);
-    // 挂起当前协程，转换为 READY 状态
+    // 挂起当前协程，转换为 READY 状态，等待下一次调度
     static void YieldToReady();
-    // 挂起当前协程，转换为 HOLD 状态
+    // 挂起当前协程，转换为 HOLD 状态，等待下一次调度
     static void YieldToHold();
     // 获取存在的协程数量
     static uint64_t TotalFiber();
+    // 获取协程 id
+    static uint64_t GetFiberID();
     // 协程入口函数
     static void MainFunc();
 
@@ -70,6 +83,25 @@ private:
     // 协程执行函数
     FiberFunc m_callback;
 };
+
+namespace FiberInfo
+{
+
+// 最后一个协程的 id
+static std::atomic_uint64_t s_fiber_id{0};
+// 存在的协程数量
+static std::atomic_uint64_t s_fiber_count{0};
+
+// 当前协程
+static thread_local Fiber* t_fiber = nullptr;
+// 主协程
+static thread_local Fiber::ptr t_master_fiber{};
+
+// 协程栈大小配置项
+static ConfigVar<uint64_t>::ptr g_fiber_stack_size =
+    Config::Lookup<uint64_t>("fiber.stack_size", 1024 * 1024);
+} // namespace FiberInfo
+
 } // namespace zjl
 
 #endif
