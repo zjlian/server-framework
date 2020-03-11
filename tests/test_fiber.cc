9900619d739b79090414c74ef6e2585beb8911f7
zjlian <zjlianzjlianzjlian@outlook.com>
2020-03-12 00:12:39 +0800

    test

--- tests/test_fiber.cc
+++ tests/test_fiber.cc
@@ -15,7 +15,6 @@ void fiberFunc()
         a = b;
         b = fib;
         // 挂起当前协程
-//        zjl::Fiber::GetThis()->swapOut();
         zjl::Fiber::YieldToReady();
     }
     std::cout << "fiberFunc() 结束" << std::endl;
@@ -24,13 +23,15 @@ void fiberFunc()
 int main(int, char**)
 {
     zjl::Fiber::GetThis();
-    auto fiber = std::make_shared<zjl::Fiber>(fiberFunc);
-    std::cout << "换入协程，打印斐波那契数列" << std::endl;
-    fiber->swapIn();
-    while (fib < 100)
     {
-        std::cout << fib << " ";
+        auto fiber = std::make_shared<zjl::Fiber>(fiberFunc);
+        std::cout << "换入协程，打印斐波那契数列" << std::endl;
         fiber->swapIn();
+        while (fib < 100 && !fiber->finish())
+        {
+            std::cout << fib << " ";
+            fiber->swapIn();
+        }
     }
     std::cout << "完成" << std::endl;
 
