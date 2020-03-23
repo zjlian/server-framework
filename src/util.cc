6c7ff31d226e2b1cb953cea344e79b894a4ccdb6
zjlian <zjlianzjlianzjlian@outlook.com>
2020-03-24 02:18:38 +0800

    feat: Backtrace 函数增加函数类型信息解析

--- src/util.cc
+++ src/util.cc
@@ -2,7 +2,8 @@
 #include "fiber.h"
 #include <execinfo.h>
 #include <iostream>
-#include <sstream>
+#include <cxxabi.h>
+
 
 namespace zjl
 {
@@ -26,21 +27,53 @@ void Backtrace(std::vector<std::string>& out, int size, int skip)
     {
         std::cerr << "Backtrace() exception, 调用栈获取失败" << std::endl;
     }
-    for (int i = skip; i < call_stack_count; i++)
+    for (int i = skip; string_list && i < call_stack_count; i++)
     {
-        // TODO 解码类型信息
         /**
+         * 解码类型信息
          * 例如一个栈信息 ./test_exception(_Z2fni+0x62) [0x564e8a313317]
-         * 函数签名在符号 "(" 前 "+" 后，
+         * 函数签名在符号 "(" 后 "+" 前，
          * 调用 abi::__cxa_demangle 进行编码转换
         */
-        out.push_back(string_list[i]);
+        std::stringstream ss;
+        char* str = string_list[i];
+        char* brackets_pos = nullptr;
+        char* plus_pos = nullptr;
+        // 找到左括号的位置
+        for (brackets_pos = str; *brackets_pos != '(' && *brackets_pos; brackets_pos++)
+        { /* do nothing */
+        }
+        assert(*brackets_pos == '(');
+        // 先把到左括号的字符串塞进字符串流里
+        *brackets_pos = '\0';
+        ss << string_list[i] << '(';
+        *brackets_pos = '(';
+        // 找到加号的位置
+        for (plus_pos = brackets_pos; *plus_pos != '+' && *plus_pos; plus_pos++)
+        { /* do nothing */
+        }
+        // 解析类型信息
+        char* type = nullptr;
+        if (*brackets_pos + 1 != *plus_pos)
+        {
+            *plus_pos = '\0';
+            int status = 0;
+            type = abi::__cxa_demangle(brackets_pos + 1, nullptr, nullptr, &status);
+            assert(status == 0 || status == -2);
+            // 当 status == -2 时，意思是字符串解析错误，直接将原字符串塞进流里
+            ss << (status == 0 ? type : brackets_pos + 1);
+            *plus_pos = '+';
+        }
+        // 把剩下的也塞进去
+        ss << plus_pos;
+        out.push_back(ss.str());
+        free(type);
     }
     // backtrace_symbols() 返回 malloc 分配的内存指针，需要 free
     free(string_list);
     free(void_ptr_list);
 }
-std::string BacktraceToSring(int size, int skip)
+std::string BacktraceToString(int size, int skip)
 {
     std::vector<std::string> call_stack;
     Backtrace(call_stack, size, skip);
